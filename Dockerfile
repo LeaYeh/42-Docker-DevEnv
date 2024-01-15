7fceb682908643d0688105ca9debf12749dd70c2
Lea Yeh <lyeh@student.42vienna.com>
2024-01-15 21:15:48 +0100

    rec: Container keep running and with user as sudoer

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 FROM ubuntu:latest
 
 # Update the system and install utils
-RUN apt-get update && apt-get install -y \
+RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y \
     sudo \
     valgrind \
     build-essential \
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 # curl -fsSL https://raw.githubusercontent.com/LeaYeh/42-Docker-DevEnv/master/Dockerfile -o Dockerfile
 # curl -fsSL https://raw.githubusercontent.com/LeaYeh/42-Docker-DevEnv/master/docker-compose.yml -o docker-compose.yml
 
-# docker build --build-arg USER=$USER -t develop-env .
-docker build --progress=plain --build-arg USER=$USER -t develop-env .
+docker build --build-arg USER=$USER -t develop-env .
+# docker build --progress=plain --build-arg USER=$USER -t develop-env .
 # docker exec -it -u leayeh <container_id> /bin/bash
 docker-compose up -d run-container
