b659629940184f9bb827b6fa6315626daa71a6e2
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2025-01-08 01:45:09 +0100

    improve: Update the run script for pure Docker container

--- run.sh
+++ run.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
+# Download latest Dockerfile and docker-compose.yml
 curl -fsSL https://raw.githubusercontent.com/LeaYeh/42-Docker-DevEnv/main/Dockerfile -o Dockerfile
 curl -fsSL https://raw.githubusercontent.com/LeaYeh/42-Docker-DevEnv/main/docker-compose.yml -o docker-compose.yml
 
-docker build --build-arg USER=$USER -t develop-env .
-# docker build --progress=plain --build-arg USER=$USER -t develop-env .
-# docker exec -it -u leayeh <container_id> /bin/bash
+# Set environment variable for current user
 echo "USER=$(whoami)" > .env
+
+# Create required directories and files
+mkdir -p "${HOME}/.ssh"
+touch "${HOME}/.gitconfig" "${HOME}/.zshrc"
+
+# Build and run the container
 docker-compose up --build -d run-container
+
+# Print helpful message
+echo "Container is running! To attach to it, use:"
+echo "docker exec -it \$(docker-compose ps -q run-container) /bin/zsh"
+echo
+echo "Check the README how to attach to the container with VS Code."
