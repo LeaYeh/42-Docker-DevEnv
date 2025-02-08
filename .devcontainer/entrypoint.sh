07157000d2664567f3f089053b9387ffcc3676cd
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2025-02-08 22:58:16 +0100

    fix: Ensure francinette aliases exist if user uses their own rc files

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -11,4 +11,19 @@ for item_path in "$mount_dir"/*; do
 done
 shopt -u dotglob nullglob
 
+# Set up francinette aliases
+rc_files=( "$HOME/.bashrc" "$HOME/.config/fish/config.fish" "$HOME/.zshrc" )
+for rc_file in "${rc_files[@]}"; do
+    mkdir -p "$(dirname "$rc_file")"
+    if ! grep "francinette=" "$rc_file" &> /dev/null; then
+        printf "\nalias francinette=%s/francinette/tester.sh\n" "$HOME" >> "$rc_file"
+    fi
+    if ! grep "paco=" "$rc_file" &> /dev/null; then
+        printf "\nalias paco=%s/francinette/tester.sh\n" "$HOME" >> "$rc_file"
+    fi
+    first_level=${rc_file#"$HOME/"}
+    first_level=${first_level%%/*}
+    chown -R "$USER:$USER" "$HOME/$first_level"
+done
+
 exec "$@"
